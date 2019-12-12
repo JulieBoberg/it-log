9045efb049e4f2398ab54c18ee029f7e88f9d324
JulieBoberg <43385336+JulieBoberg@users.noreply.github.com>
2019-12-12 13:13:49 -0800

    Log Modal and Edit Modal

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Fragment, useEffect } from "react";
 import Searchbar from "./components/layout/Searchbar";
 import Logs from "./components/logs/Logs";
+import AddBtn from "./components/layout/AddBtn";
+import AddLogModal from "./components/logs/AddLogModal";
+import EditLogModal from "./components/logs/EditLogModals";
 
 import "materialize-css/dist/css/materialize.min.css";
 import M from "materialize-css/dist/js/materialize.min.js";
@@ -17,6 +20,9 @@ const App = () => {
     <Fragment>
       <Searchbar />
       <div className='container'>
+        <AddBtn />
+        <AddLogModal />
+        <EditLogModal />
         <Logs />
       </div>
     </Fragment>
